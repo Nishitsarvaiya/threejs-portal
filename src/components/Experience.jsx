9a07ac6162ca27f4309887d0bda52da23fc52251
Nishit Sarvaiya <nishitsarvaiiya@gmail.com>
2024-01-21 20:36:38 +0530

    minor fix

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,11 +1,11 @@
-import { CameraControls } from "@react-three/drei";
-import Door from "./Door";
-import Ground from "./Ground";
-import { useThree } from "@react-three/fiber";
-import { useEffect, useRef, useState } from "react";
-import { Vector3 } from "three";
-import Russian_soldier from "./Russian_soldier";
-import { isTouchEnabledDevice } from "../helpers";
+import { CameraControls } from '@react-three/drei';
+import Door from './Door';
+import Ground from './Ground';
+import { useThree } from '@react-three/fiber';
+import { useEffect, useRef, useState } from 'react';
+import { Vector3 } from 'three';
+import Russian_soldier from './Russian_soldier';
+import { isTouchEnabledDevice } from '../helpers';
 
 export const Experience = () => {
 	const [active, setActive] = useState(null);
@@ -23,9 +23,9 @@ export const Experience = () => {
 			controlsRef.current.minPolarAngle = 0;
 		} else {
 			if (isMobile) {
-				controlsRef.current.setPosition(30, 12, 30, true);
+				controlsRef.current.setLookAt(30, 12, 30, 0, 0, 0, true);
 			} else {
-				controlsRef.current.setPosition(16, 8, 18, true);
+				controlsRef.current.setLookAt(10, 8, 12, 0, 0, 0, true);
 			}
 			controlsRef.current.maxPolarAngle = Math.PI / 2;
 			controlsRef.current.minPolarAngle = 0;
@@ -35,10 +35,10 @@ export const Experience = () => {
 	return (
 		<>
 			<CameraControls maxPolarAngle={1.45} ref={controlsRef} smoothTime={0.36} dollyDragInverted />
-			<color args={["#000"]} attach="background" />
+			<color args={['#000']} attach='background' />
 			{/* <ambientLight intensity={0.2} /> */}
 			<spotLight
-				color={"#f6f6f6"}
+				color={'#f6f6f6'}
 				intensity={3}
 				angle={1}
 				penumbra={0.5}
@@ -53,33 +53,36 @@ export const Experience = () => {
 				args={[4, 6, 0.1]}
 				position={[0, 3, 7]}
 				rotation-y={Math.PI}
-				env="city"
-				texture="world2"
-				name="AR-15"
+				env='city'
+				texture='world2'
+				name='AR-15'
 				active={active}
-				setActive={setActive}>
+				setActive={setActive}
+			>
 				{/* <AR15 scale={0.5} rotation-y={Math.PI / 2} /> */}
 			</Door>
 			<Door
 				args={[4, 6, 0.1]}
 				position={[6, 3, -4]}
 				rotation-y={Math.PI / -3}
-				env="dawn"
-				texture="world3"
-				name="AK-47s"
+				env='dawn'
+				texture='world3'
+				name='AK-47s'
 				active={active}
-				setActive={setActive}>
+				setActive={setActive}
+			>
 				{/* <AK47 scale={0.3} rotation-y={Math.PI / 2} /> */}
 			</Door>
 			<Door
 				args={[4, 6, 0.1]}
 				position={[-6, 3, -4]}
 				rotation-y={Math.PI / 3}
-				env="apartment"
-				texture="world4"
-				name="SMG"
+				env='apartment'
+				texture='world4'
+				name='SMG'
 				active={active}
-				setActive={setActive}>
+				setActive={setActive}
+			>
 				{/* <SMG scale={0.7} rotation-y={Math.PI / 2} position-x={-0.2} /> */}
 			</Door>
 			<Russian_soldier />
